9af3b48bedadf0231ae14f03971ebc1a44cf10fd
Jose Joel Maldonado Alvarado <joelalvaradom02@gmail.com>
2025-09-04 13:27:46 -0600

    Proyecto Prueba Técnica

--- android/app/src/main/java/com/rnapp/MainActivity.kt
+++ android/app/src/main/java/com/rnapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.lidos
+package com.rnapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/rnapp/MainApplication.kt
+++ android/app/src/main/java/com/rnapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.lidos
+package com.rnapp
 
 import android.app.Application
 import com.facebook.react.PackageList
