5c7dcdfb0243e52f648667916eebb7bf86c6f3e6
Jose Joel Maldonado Alvarado <joelalvaradom02@gmail.com>
2025-09-04 13:27:28 -0600

    Proyecto Prueba Técnica

--- ios/rnappTests/rnappTests.m
+++ ios/rnappTests/rnappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface lidosTests : XCTestCase
+@interface rnappTests : XCTestCase
 
 @end
 
-@implementation lidosTests
+@implementation rnappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
