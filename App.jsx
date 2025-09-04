bfbcf3703b4b4c648de73074f3be16bfa0f449bb
Jose Joel Maldonado Alvarado <joelalvaradom02@gmail.com>
2025-09-04 15:51:25 -0600

    feat(app): App Main

--- App.jsx
+++ App.jsx
@@ -1,12 +1,39 @@
-import { Text } from "react-native";
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { apolloClient } from './src/apollo/client';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import CharactersScreen from './src/screens/CharactersScreen';
+import FavoritesScreen from './src/screens/FavoritesScreen';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faUsers, faHeart } from '@fortawesome/free-solid-svg-icons';
 
-/** Componente principal de la aplicación */
-const App = () => {
+const Tab = createBottomTabNavigator();
+export default function App() {
     return (
-        <>
-            <Text>Hola</Text>
-        </>
+        <ApolloProvider client={apolloClient}>
+            <PaperProvider>
+                <NavigationContainer>
+                    <Tab.Navigator
+                        screenOptions={({ route }) => ({
+                            headerShown: true,
+                            tabBarIcon: ({ size, color }) => {
+                                let icon;
+                                if (route.name === 'Personajes') {
+                                    icon = faUsers;
+                                } else {
+                                    icon = faHeart;
+                                }
+                                return <FontAwesomeIcon icon={icon} size={size} color={color} />;
+                            },
+                        })}
+                    >
+                        <Tab.Screen name="Personajes" component={CharactersScreen} />
+                        <Tab.Screen name="Favoritos" component={FavoritesScreen} />
+                    </Tab.Navigator>
+                </NavigationContainer>
+            </PaperProvider>
+        </ApolloProvider>
     );
 }
-
-export default App;
